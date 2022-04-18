95a9698d00635bacae8030bf3f437bd8ff14894a
ChloeBoistel <alu0100788020@ull.edu.es>
2022-04-18 18:52:55 +0000

    despliege5

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "Mi primer post: Aprendiendo a usar un Generador Estático",
+        "excerpt":"Por fin es Jueves   Ya queda menos para el fin de semana  ","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/aet",
+        "teaser": null
+      },{
+        "title": "Clase",
+        "excerpt":"Somos 20 alumnos   La respuesta a cuánto vale chuchu es 4   Hola criaturitas del señor   Ay Dios mío, cuánto de semana queda…   I am a fabulous person.   ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/clase",
+        "teaser": null
+      },{
+        "title": "Mi segundo post",
+        "excerpt":"Un día tranquilo           Me gusta mucho comer naranjas            Me gusta mucho comer peras            Me gusta mucho comer limones            Me gusta mucho comer fresas          ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/aet",
+        "teaser": null
+      },{
+        "title": "Un buen día para vivir",
+        "excerpt":"Buenos días   Dar un paseo relajante  ","categories": ["personal"],
+        "tags": [],
+        "url": "/personal/personal2",
+        "teaser": null
+      },{
+        "title": "Aprendiendo a poner fórmulas de LaTeX",
+        "excerpt":"\\[a \\times x^2 = y_j\\] \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\] Esto de aquí son datos csv en formato Jason: {“First name”=&gt;”John”, “Last name”=&gt;”Doe”, “Age”=&gt;”35”, “Location”=&gt;”United States”}{“First name”=&gt;”Jane”, “Last name”=&gt;”Doe”, “Age”=&gt;”29”, “Location”=&gt;”France”}{“First name”=&gt;”Jack”, “Last name”=&gt;”Hill”, “Age”=&gt;”25”, “Location”=&gt;”Australia”} Y ahora ponemos la tabla: First name Last name...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/latex",
+        "teaser": null
+      },{
+        "title": "Hola",
+        "excerpt":"¡Hola a todos!   Que tengan un buen maravilloso día  ","categories": ["personal"],
+        "tags": [],
+        "url": "/personal/personal1",
+        "teaser": null
+      },{
+        "title": "Reflexión",
+        "excerpt":"Desigualdad de oportunidades en la educación Frente a perseguir la igualdad de oportunidades, que solo se daría probablemente en el caso de que no existiera desigualdad de recursos en las familias o que, directamente, no existieran las familias; el objetivo debe ser lograr la equidad en una sociedad y en...","categories": ["proyecto"],
+        "tags": [],
+        "url": "/proyecto/proyecto1",
+        "teaser": null
+      },{
+        "title": "Familias",
+        "excerpt":"El tipo de familia no condiciona la educación y desarrollo del infante Durante las últimas décadas, el concepto de familia ha tenido que ir cambiando y evolucionando a la vez que lo hacía la sociedad, adecuándose el vocabulario a las nuevas realidades que aparecen. Se ha pasado de considerar un...","categories": ["proyecto"],
+        "tags": [],
+        "url": "/proyecto/proyecto2",
+        "teaser": null
+      }]
